8db7db6a4f9758387d61a26f1fb1681834860fbb
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-03-02 07:28:24 -0400

    Se configuró la actualización del listado de Todos cuando se realiza una actualización

--- src/todos/components/TodoItem.tsx
+++ src/todos/components/TodoItem.tsx
@@ -8,14 +8,16 @@ import { IoCheckboxOutline, IoSquareOutline } from 'react-icons/io5';
 interface Props {
   todo: Todo;
   // TODO: Acciones que quiero llamar
+  toggleTodo: (id: string, complete: boolean) => Promise<Todo|void>
 }
 
-export const TodoItem = ({ todo }: Props) => {
+export const TodoItem = ({ todo, toggleTodo }: Props) => {
   return (
     <div className={ todo.complete ? styles.todoDone : styles.todoPending }>
       <div className="flex flex-col sm:flex-row justify-start items-center gap-4">
 
         <div
+          onClick={ () => toggleTodo(todo.id, !todo.complete) }
           className={`
             flex p-2 rounded-md cursor-pointer
             hover:bg-opacity-60
---
+++ src/todos/helpers/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "@prisma/client";
+
+export const updateTodo = async( id: string, complete: boolean ):Promise<Todo> => {
+
+    const body = { complete };
+  
+    const todo = await fetch(`/api/todos/${ id }`,{
+      method: 'PUT',
+      body: JSON.stringify( body ),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }).then( res => res.json() );
+    
+    console.log({todo});
+  
+    return todo;
+  }
+  
