3aa57407e1332703e88a2e50c7cdecb4d1305f50
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-03-03 06:57:38 -0400

    Se creó la funcionalidad para crear y eliminar con server actions

--- src/todos/actions/todo-actions.ts
+++ src/todos/actions/todo-actions.ts
@@ -30,3 +30,25 @@ export const toggleTodo = async( id: string, complete: boolean ): Promise<Todo> 
   return updatedTodo;
 
 }
+
+export const addTodo = async( description: string ) => {
+  
+  try {
+    const todo = await prisma.todo.create({ data: { description } })
+    revalidatePath('/dashboard/server-todos');
+    
+    return todo;
+
+  } catch (error) {
+    return {
+      message: 'Error creando todo'
+    }
+  }
+}
+
+export const deleteCompleted = async():Promise<void> => {
+  
+  await prisma.todo.deleteMany({ where: { complete: true } });
+  revalidatePath('/dashboard/server-todos');
+  
+}
