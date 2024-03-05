e49c81cc1815a5a176e9dcca0898eeb6f6983fe7
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-03-05 10:22:59 -0400

    Se configuró next-auth y el login con github

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,34 @@
-import { WidgetItem } from "@/components";
+// import { WidgetItem } from "@/components";
 
-export default function DashboardPage() {
+import { WidgetItem } from "@/components";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+
+  const session = await getServerSession(authOptions);
+
+  if ( !session ) {
+    redirect('/api/auth/signin');
+  }
+
   return (
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      <WidgetItem />
+    <div className="grid gap-6 grid-cols-1 ">
+
+
+      <WidgetItem title="Usuario conectado S-Side">
+        <div className="flex flex-col">
+          <span>{ session.user?.name }</span>
+          <span>{ session.user?.image }</span>
+          <span>{ session.user?.email }</span>
+          <hr/>
+          <div className="py-4">
+            { JSON.stringify( session ) }
+          </div>
+        </div>
+      </WidgetItem>
+      
     </div>
   );
 }
