b30351dbe1888693679e7deb06a772b5837fd1fd
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-03-06 14:49:02 -0400

    Se configuró next-auth y el login con google

--- next.config.mjs
+++ next.config.mjs
@@ -9,7 +9,11 @@ const nextConfig = {
         {
           protocol: 'https',
           hostname: 'avatars.githubusercontent.com'
-        }
+        },
+        {
+          protocol: 'https',
+          hostname: 'lh3.googleusercontent.com'
+        }       
       ]
     }
   }
