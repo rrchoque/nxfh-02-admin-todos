2de7c77b9b160dc4722c4343f6acce3f5fe31f99
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-03-06 06:05:40 -0400

    Se configuró la opción para mostrar la información del usuario en el lado del servidor

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -4,6 +4,8 @@ import { CiLogout } from 'react-icons/ci';
 import { SidebarItem } from './SidebarItem';
 import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoListOutline } from 'react-icons/io5';
 import { LuCookie } from 'react-icons/lu';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
 
 const menuItems = [
   {
@@ -33,7 +35,16 @@ const menuItems = [
   },
 ]
 
-export const Sidebar = () => {
+export const Sidebar = async () => {
+  const session = await getServerSession(authOptions);
+
+  const avatarUrl = ( session?.user?.image )
+  ? session.user.image
+  : 'https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp';
+
+  const userName = session?.user?.name ?? 'No Name';
+  //const userRoles = session?.user?.roles ?? ['client'];
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -53,13 +64,13 @@ export const Sidebar = () => {
         <div className="mt-8 text-center">
          
           <Image 
-            src="https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp" 
+            src={ avatarUrl } 
             width={150}
             height={150}
             alt="" 
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28" 
           />
-          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
+          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{ userName }</h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
 
