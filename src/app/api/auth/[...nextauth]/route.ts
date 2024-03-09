e67f234bf31928bb9741c91b799c944b3bf4fc94
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-03-09 10:48:16 -0400

    Se agregó el login y registro mediante usuario y password

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,8 @@ import NextAuth, { NextAuthOptions } from "next-auth"
 import { Adapter } from "next-auth/adapters";
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials";
+import { signInEmailPassword } from "@/auth/actions/auth-actions";
 
 export const authOptions:NextAuthOptions = {
   adapter: PrismaAdapter( prisma ) as Adapter,
@@ -15,6 +17,24 @@ export const authOptions:NextAuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID ?? '',
       clientSecret: process.env.GITHUB_SECRET ?? '',
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Correo electrónico", type: "email", placeholder: "correo electrónico" },
+        password: { label: "Contraseña", type: "password", placeholder: '******' }
+      },
+      async authorize(credentials, req) {
+        // Add logic here to look up the user from the credentials supplied
+        const user = await signInEmailPassword(credentials!.email, credentials!.password );
+  
+        if (user) {
+          // Any object returned will be saved in `user` property of the JWT
+          return user;
+        } 
+
+        return null;
+      }
     }),
   ],
   session: {
