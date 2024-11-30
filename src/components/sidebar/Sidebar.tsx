357adddd83f0439363c43adf461081c3a88c6dc2
Reynaldo Rodrigo Choque Vicente <reynaldorodrigochoque@gmail.com>
2024-11-30 07:00:43 -0400

    Se actualizaron imagenes porque los enlaces ya no estaban disponibles

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,55 +1,61 @@
-import Image from 'next/image';
-import Link from 'next/link';
-import { CiLogout } from 'react-icons/ci';
-import { SidebarItem } from './SidebarItem';
-import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoListOutline, IoPersonOutline } from 'react-icons/io5';
-import { LuCookie } from 'react-icons/lu';
-import { getServerSession } from 'next-auth';
-import { authOptions } from '@/app/api/auth/[...nextauth]/route';
-import { LogoutButton } from '..';
+import Image from "next/image";
+import Link from "next/link";
+import { CiLogout } from "react-icons/ci";
+import { SidebarItem } from "./SidebarItem";
+import {
+  IoBasketOutline,
+  IoCalendarOutline,
+  IoCheckboxOutline,
+  IoListOutline,
+  IoPersonOutline,
+} from "react-icons/io5";
+import { LuCookie } from "react-icons/lu";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { LogoutButton } from "..";
 
 const menuItems = [
   {
     icon: <IoCalendarOutline />,
-    title: 'Dashboard',
-    path: '/dashboard'
+    title: "Dashboard",
+    path: "/dashboard",
   },
   {
     icon: <IoCheckboxOutline />,
-    title: 'Rest TODOS',
-    path: '/dashboard/rest-todos'
+    title: "Rest TODOS",
+    path: "/dashboard/rest-todos",
   },
   {
     icon: <IoListOutline />,
-    title: 'Server Actions',
-    path: '/dashboard/server-todos'
+    title: "Server Actions",
+    path: "/dashboard/server-todos",
   },
   {
     icon: <LuCookie />,
-    title: 'Cookies',
-    path: '/dashboard/cookies'
+    title: "Cookies",
+    path: "/dashboard/cookies",
   },
   {
     icon: <IoBasketOutline />,
-    title: 'Productos',
-    path: '/dashboard/products'
+    title: "Productos",
+    path: "/dashboard/products",
   },
   {
     icon: <IoPersonOutline />,
-    title: 'Perfil',
-    path: '/dashboard/profile'
+    title: "Perfil",
+    path: "/dashboard/profile",
   },
-]
+];
 
 export const Sidebar = async () => {
   const session = await getServerSession(authOptions);
 
-  const avatarUrl = ( session?.user?.image )
-  ? session.user.image
-  : 'https://tailus.io/sources/blocks/stats-cards/preview/images/second_user.webp';
+  const avatarUrl = session?.user?.image
+    ? session.user.image
+    : "/images/user.svg";
 
-  const userName = session?.user?.name ?? 'No Name';
-  const userRoles = session?.user?.roles ?? ['client'];
+  const userName = session?.user?.name ?? "No Name";
+  const userRoles = session?.user?.roles ?? ["client"];
 
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
@@ -58,9 +64,10 @@ export const Sidebar = async () => {
           {/* TODO: Next/Link hacia dashboard */}
           <Link href="#" title="home">
             {/* Next/Image */}
-            <Image src="https://tailus.io/sources/blocks/stats-cards/preview/images/logo.svg" 
-              className="w-32" 
-              alt="tailus logo" 
+            <Image
+              src="/images/logo-reynaldo.png"
+              className="w-32"
+              alt="tailus logo"
               width={150}
               height={150}
             />
@@ -68,27 +75,25 @@ export const Sidebar = async () => {
         </div>
 
         <div className="mt-8 text-center">
-         
-          <Image 
-            src={ avatarUrl } 
+          <Image
+            src={avatarUrl}
             width={150}
             height={150}
-            alt="" 
-            className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28" 
+            alt=""
+            className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28 bg-cyan-50"
           />
-          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{ userName }</h5>
+          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">
+            {userName}
+          </h5>
           <span className="hidden text-gray-400 lg:block capitalize">
-          { userRoles.join(',') }
+            {userRoles.join(",")}
           </span>
         </div>
 
         <ul className="space-y-2 tracking-wide mt-8">
-          {
-            menuItems.map( item => (
-              <SidebarItem key={ item.path } {...item} />
-            ))
-          }
-          
+          {menuItems.map((item) => (
+            <SidebarItem key={item.path} {...item} />
+          ))}
         </ul>
       </div>
 
@@ -96,5 +101,5 @@ export const Sidebar = async () => {
         <LogoutButton />
       </div>
     </aside>
-  )
-}
+  );
+};
